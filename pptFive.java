4d6096b9eb3da9b688ea9a1ebdb8f8fc61356dc7
Sagar01Dev <104485238+Sagar01Dev@users.noreply.github.com>
2023-07-04 22:09:32 +0530

    Add files via upload
    PPT MOCK TEST2 QUE5

---
+++ pptFive.java
@@ -0,0 +1,14 @@
+package MockTest2PPT;
+
+public class pptFive {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 1.75;
+        String name = "Iron Man";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+    }
+}
+
