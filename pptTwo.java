c81201629b60e64e39bdcbc7dd260a0f526bf379
Sagar01Dev <104485238+Sagar01Dev@users.noreply.github.com>
2023-07-04 22:05:45 +0530

    Add files via upload
    PPT MOCK TEST QUE 2

---
+++ pptTwo.java
@@ -0,0 +1,17 @@
+package MockTest2PPT;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class pptTwo{
+    public static void main(String[] args) {
+       int num =10;
+       int series =0;
+
+
+       for(int i =0;i<=num;i++){
+           
+           System.out.println(series +=  i );
+       }
+
+    }
+}
